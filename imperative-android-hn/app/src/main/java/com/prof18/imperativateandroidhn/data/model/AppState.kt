46f1f97aec74e1ed253da3cc0462b405943f6731
Marco Gomiero <marco.gomiero.93@gmail.com>
2020-02-11 09:15:06 +0100

    Start working on android imperative example

---
+++ imperative-android-hn/app/src/main/java/com/prof18/imperativateandroidhn/data/model/AppState.kt
@@ -0,0 +1,5 @@
+package com.prof18.imperativateandroidhn.data.model
+
+data class AppState(
+    var newsState: NewsState
+)
---
+++ imperative-android-hn/app/src/main/java/com/prof18/imperativateandroidhn/data/model/News.kt
@@ -0,0 +1,23 @@
+package com.prof18.imperativateandroidhn.data.model
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+data class News(
+    val by: String,
+    val id: Long,
+    val score: Int,
+    val text: String? = null,
+    val time: Long,
+    val title: String,
+    val type: String,
+    val url: String? = null
+) {
+
+    fun getStringTime(): String {
+        val formatter = SimpleDateFormat("d MMM yyyy", Locale.getDefault())
+        return formatter.format(Date(time*1000))
+
+    }
+
+}
